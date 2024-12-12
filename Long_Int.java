753734d6b4829a800185a1ae6f6f986d3558e940
Tejasree-S27 <stejasree348@gmail.com>
2024-12-12 20:23:22 +0530

    Add files via upload

---
+++ Long_Int.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Long_Int 
+{
+    public static void main(String[] args)
+    {
+        long ans=90;
+        Scanner sc=new Scanner(System.in);
+        //accepting long-int
+        long s=sc.nextInt();
+        System.out.println(ans);
+        System.out.println(s);
+    }
+}
