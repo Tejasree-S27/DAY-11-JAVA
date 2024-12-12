ff013156549a2895cfadb36908d19c0c0debac04
Tejasree-S27 <stejasree348@gmail.com>
2024-12-12 20:01:38 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ArrayList
+{
+    public static void main(String[] args) {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(10);
+        a.add(20);
+        a.add(30);
+        System.out.println(a);
+    }
+}
