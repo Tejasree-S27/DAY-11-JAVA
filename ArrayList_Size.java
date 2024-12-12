71abab5076a8c0eb86602fe275aea4bf300a53ff
Tejasree-S27 <stejasree348@gmail.com>
2024-12-12 21:19:56 +0530

    Add files via upload

---
+++ ArrayList_Size.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ArrayList_Size 
+{
+    public static void main(String args[])
+    {
+        ArrayList<Integer> b = new ArrayList<>();  
+        b.add(12);
+        b.add(23);
+        b.add(45);
+        // Print the size of the ArrayList 'b'
+        System.out.println(b.size());  
+    }
+}
