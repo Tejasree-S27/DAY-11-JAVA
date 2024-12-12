b4aeda88df9e51a0ea5208eee426ce4e670416ff
Tejasree-S27 <stejasree348@gmail.com>
2024-12-12 21:05:15 +0530

    Add files via upload

---
+++ PrintingDouble.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class PrintingDouble 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        //accepting double
+        double s=sc.nextDouble();
+        //printing double value
+        System.out.println(s);
+    }
+}
